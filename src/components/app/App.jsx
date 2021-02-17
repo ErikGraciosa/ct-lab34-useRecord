a5453a2322c0ebb2e447a767d025c57c2f3e571d
ErikGraciosa <erikgraciosa@gmail.com>
2021-02-17 07:59:43 -0800

    tests written

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -38,11 +38,22 @@ function App() {
       <div>Color Changer</div>
       <button onClick={undo}>undo</button>
       <button onClick={redo}>redo</button>
+      
       <input 
+        data-testid="selector"
+        name="selector"
         type="color" 
         value={current} 
         onChange={({ target }) => record(target.value)} />
-      <div style={{ backgroundColor: current, width: '10rem', height: '10rem' }}></div>
+      
+      
+      <div 
+        data-testid="output" 
+        style={{ 
+          backgroundColor: current, 
+          width: '10rem', 
+          height: '10rem' 
+        }}></div>
     
     </>
   );
--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { 
+  screen,
+  render,
+  cleanup,
+  fireEvent,
+  waitFor } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
@@ -8,4 +13,90 @@ describe('App component', () => {
     const { asFragment } = render(<App />);
     expect(asFragment()).toMatchSnapshot();
   });
+
+  it('verifies that color changes based on input', () => {
+    render(<App />);
+
+    const selector = screen.getByTestId('selector');
+    const output = screen.getByTestId('output');
+
+    fireEvent.change(selector, {
+      target: {
+        value: '#FF0000'
+      }
+    });
+
+    return waitFor(() => {
+      expect(output).toHaveStyle({
+        backgroundColor: '#FF0000'
+      });
+    });
+
+  });
+
+  it('verifies that color changes based on undo to previous', () => {
+    render(<App />);
+
+    const selector = screen.getByTestId('selector');
+    const output = screen.getByTestId('output');
+    const undo = screen.getByText('undo');
+
+    fireEvent.change(selector, {
+      target: {
+        value: '#FF0000'
+      }
+    });
+    fireEvent.change(selector, {
+      target: {
+        value: '#00FF00'
+      }
+    });
+    fireEvent.change(selector, {
+      target: {
+        value: '#0000FF'
+      }
+    });
+    fireEvent.click(undo);
+
+    return waitFor(() => {
+      expect(output).toHaveStyle({
+        backgroundColor: '#00FF00'
+      });
+    });
+  });
+
+  it('verifies color changes based on redo to next index color', () => {
+    render(<App />);
+
+    const selector = screen.getByTestId('selector');
+    const output = screen.getByTestId('output');
+    const undo = screen.getByText('undo');
+    const redo = screen.getByText('redo');
+
+    fireEvent.change(selector, {
+      target: {
+        value: '#FF0000'
+      }
+    });
+    fireEvent.change(selector, {
+      target: {
+        value: '#00FF00'
+      }
+    });
+    fireEvent.change(selector, {
+      target: {
+        value: '#0000FF'
+      }
+    });
+    fireEvent.click(undo);
+    fireEvent.click(undo);
+    fireEvent.click(redo);
+
+    return waitFor(() => {
+      expect(output).toHaveStyle({
+        backgroundColor: '#00FF00'
+      });
+    });
+  });
+
 });
